fdca2071b352331bfff2933da59f7942c2f991bd
gabit <gabit690@gmail.com>
2021-02-10 10:03:39 -0300

    Estructura inicial del proyecto

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+import { Component } from "react";
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default Calculator;
