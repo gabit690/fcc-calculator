b736c8c8b2d903308144d973092d4ab33a37022e
gabit <gabit690@gmail.com>
2021-02-11 12:33:17 -0300

    Estructuracion basica de los elementos

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,13 +1,30 @@
+import { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import './Calculator.css';
 
-import { Component } from "react";
+import CalculatorScreen from '../CalculatorScreen/CalculatorScreen';
+import CalculatorPad from '../CalculatorPad/CalculatorPad';
 
 class Calculator extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      on: true,
+      resultExpression: "resultado",
+      currentIntput: "inputs"
+    };
+  }
+
   render() {
     return (
-      <div>
-          
+      <div id="calculator-container" className="container-fluid">
+        <CalculatorScreen 
+          result={this.state.resultExpression}
+          input={this.state.currentIntput}
+        />
+        <CalculatorPad />
       </div>
     );
   }
