9741a8e32ebbb96b14ced9de05863825758396a1
gabit <gabit690@gmail.com>
2021-02-16 11:25:08 -0300

    Funcionalidad para el paso de los test del proyecto

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,6 +6,13 @@ import './Calculator.css';
 import CalculatorScreen from '../CalculatorScreen/CalculatorScreen';
 import CalculatorPad from '../CalculatorPad/CalculatorPad';
 
+Number.prototype.getPrecision = function() {
+  var s = this + "",
+      d = s.indexOf('.') + 1;
+
+  return !d ? 0 : s.length - d;
+};
+
 class Calculator extends Component {
 
   constructor() {
@@ -24,15 +31,13 @@ class Calculator extends Component {
 
     switch (buttonKey) {
       case "C":
-        console.log("CLEAR");
         this.cleanScreen();
         break;
       case "P":
-        console.log("POWER");
         this.switchPower();
         break;
       case "=":
-        console.log("EQUAL");
+        this.makeOperation("=");
         break;
       default:
         this.enterKey(buttonKey);
@@ -55,57 +60,60 @@ class Calculator extends Component {
 
   enterKey(key) {
 
-    switch (key) {
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        this.inputNumber(key);
-        break;
-      case ".":
-      case "/":
-      case "X":
-      case "-":
-      case "+":
-        this.inputOperator(key);
-        break;
-      case "=":
-        break;
-    }
+      switch (key) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          this.inputNumber(key);
+          break;
+        case ".":
+        case "/":
+        case "X":
+        case "-":
+        case "+":
+          this.inputOperator(key);
+          break;
+      }
+
 
   }
 
   inputNumber(number) {
 
-    if (/=/.test(this.state.resultExpression) || 
-        this.state.resultExpression == "" || 
-        this.state.currentIntput == "-0") {
-      this.setState({
-        resultExpression: number, 
-        currentIntput: number
-      });
-    } else if (/\d+[\/x\-\+]$/.test(this.state.resultExpression)) {
-      this.setState({
-        resultExpression: this.state.resultExpression + number, 
-        currentIntput: number
-      });
-    } else if (/\d+[\/x\-\+]+0$|^0$/.test(this.state.resultExpression)) {
-      this.setState({
-        resultExpression: this.state.resultExpression.replace(/.$/, number), 
-        currentIntput: number
-      });
-    } else if (/[^0]|\./.test(this.state.currentIntput)) {
-      this.setState({
-        resultExpression: this.state.resultExpression + number, 
-        currentIntput: this.state.currentIntput + number
-      });
+    if (this.state.currentIntput.length <= 15) {
+      if (/=|^\-?0$/.test(this.state.resultExpression) || 
+          this.state.resultExpression == "" || 
+          /zero/.test(this.state.currentIntput)) {
+        this.setState({
+          resultExpression: number, 
+          currentIntput: number
+        });
+      } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] $/.test(this.state.resultExpression)) {
+        this.setState({
+          resultExpression: this.state.resultExpression + number, 
+          currentIntput: number
+        });
+      } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] \-?0$/.test(this.state.resultExpression)) {
+        this.setState({
+          resultExpression: this.state.resultExpression.replace(/(\-)?.$/, number), 
+          currentIntput: number
+        });
+      } else if (/^\-|\.|^[1-9]+/.test(this.state.currentIntput)) {
+        this.setState({
+          resultExpression: this.state.resultExpression + number, 
+          currentIntput: this.state.currentIntput + number
+        });
+      }
     }
+
+
 
   }
 
@@ -124,57 +132,144 @@ class Calculator extends Component {
 
   enterDecimal() {
 
-    if (/=/.test(this.state.resultExpression) || 
-        this.state.resultExpression == "") {
-      this.setState({
-        resultExpression: "0.", 
-        currentIntput: "0."
-      });
-    } else if (/\d+[\/x\-\+]$/.test(this.state.resultExpression)) {
-      this.setState({
-        resultExpression: this.state.resultExpression + "0.", 
-        currentIntput: "0."
-      });
-    } else if (/\d+[\/x\-\+]\-$/.test(this.state.resultExpression)) {
-      this.setState({
-        resultExpression: this.state.resultExpression + "0.", 
-        currentIntput: this.state.currentIntput + "0."
-      });
-    } else if (!/\./.test(this.state.currentIntput)) {
-      this.setState({
-        resultExpression: this.state.resultExpression + ".", 
-        currentIntput: this.state.currentIntput + "."
-      });
+    if (this.state.currentIntput.length <= 14) {
+      if (/=/.test(this.state.resultExpression) || 
+          this.state.resultExpression == "" || 
+          /zero/.test(this.state.currentIntput)) {
+        this.setState({
+          resultExpression: "0.", 
+          currentIntput: "0."
+        });
+      } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] $/.test(this.state.resultExpression)) {
+        this.setState({
+          resultExpression: this.state.resultExpression + "0.", 
+          currentIntput: "0."
+        });
+      } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] \-$/.test(this.state.resultExpression)) {
+        this.setState({
+          resultExpression: this.state.resultExpression + "0.", 
+          currentIntput: this.state.currentIntput + "0."
+        });
+      } else if (!/\./.test(this.state.currentIntput)) {
+        this.setState({
+          resultExpression: this.state.resultExpression + ".", 
+          currentIntput: this.state.currentIntput + "."
+        });
+      }
     }
+
 
   }
 
+  makeOperation(pressedOperator) {
+    if (/^\-?\d+(\.?\d+)? [\/x\-\+] \-?\d+(\.?\d+)?$/.test(this.state.resultExpression)) {
+
+      const operand1 = Number(this.state.resultExpression.match(/^(\-)?\d+(\.\d+)?/)[0]);
+      const expressionOperator = this.state.resultExpression.match(/ [\/x\-\+] /)[0].trim();;
+      const operand2 = Number(this.state.resultExpression.match(/(\-)?\d+(\.\d+)?$/)[0]);
+      let newResultExpression = "";
+      let newCurrentInput = "";
+      
+      if (/=/.test(pressedOperator)) {
+
+        let resultOperation = "";
+
+        switch (expressionOperator) {
+          case "/":
+            resultOperation = (operand2 == 0) ? 
+                                "Division by zero"
+                                : 
+                                ((operand1 / operand2).getPrecision() > 4) ? 
+                                  (operand1 / operand2).toFixed(4).toString() 
+                                  : 
+                                  (operand1 / operand2).toString();
+            break;
+          case "x":
+            resultOperation = ((operand1 * operand2).getPrecision() > 4) ? 
+                                (operand1 * operand2).toFixed(4).toString() 
+                                : 
+                                (operand1 * operand2).toString();
+            break;
+          case "-":
+            resultOperation = ((operand1 - operand2).getPrecision() > 4) ? 
+                                (operand1 - operand2).toFixed(4).toString() 
+                                : 
+                                (operand1 - operand2).toString();
+            break;
+          case "+":
+            resultOperation = ((operand1 + operand2).getPrecision() > 4) ? 
+                                (operand1 + operand2).toFixed(4).toString() 
+                                : 
+                                (operand1 + operand2).toString();
+            break;
+        }
+
+        newResultExpression = this.state.resultExpression + " = " + resultOperation;
+        newCurrentInput = resultOperation;
+
+      } else {
+
+        newCurrentInput = pressedOperator;
+
+        switch (expressionOperator) {
+          case "/":
+            newResultExpression = (operand2 == 0) ? "" : (operand1 / operand2).toString() + " " + pressedOperator.toLowerCase() + " ";
+            break;
+          case "x":
+            newResultExpression = (operand1 * operand2).toString() + " " + pressedOperator.toLowerCase() + " ";
+            break;
+          case "-":
+            newResultExpression = (operand1 - operand2).toString() + " " + pressedOperator.toLowerCase() + " ";
+            break;
+          case "+":
+            newResultExpression = (operand1 + operand2).toString() + " " + pressedOperator.toLowerCase() + " ";
+            break;
+        }
+      }
+
+      if (newResultExpression != "") {
+        this.setState({
+          resultExpression: newResultExpression,
+          currentIntput: (Number(newCurrentInput).getPrecision() > 4) ? Number(newCurrentInput).toFixed(4).toString() : newCurrentInput
+        });
+      } else {
+        this.setState({
+          currentIntput: "division by zero"
+        });
+      }
+
+    }
+  }
+
   enterArithmeticOperator(operator) {
-    
-    if (/\-/.test(operator) && this.state.resultExpression == "") {
+
+    if (/\-/.test(operator) && (/zero/.test(this.state.currentIntput) || this.state.resultExpression == "")) {
       this.setState({
         resultExpression: "-0", 
         currentIntput: "-0"
       });
-    } else if (/=/.test(this.state.resultExpression) || 
-                this.state.resultExpression == "") {
+    } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] $/.test(this.state.resultExpression) && /\-/.test(operator)) {
       this.setState({
-        resultExpression: this.state.currentIntput + operator.toLowerCase(), 
+        resultExpression: this.state.resultExpression + "-", 
+        currentIntput: "-"
+      });
+    } else if (/=/.test(this.state.resultExpression)) {
+      this.setState({
+        resultExpression: this.state.currentIntput + " " + operator.toLowerCase() + " ", 
         currentIntput: operator
       });
-    } else if (!/[\/x\-\+]/.test(this.state.resultExpression) ||
-                (/\d+[\/x\+]$/.test(this.state.resultExpression)) 
-                && 
-                /\-/.test(operator)) {
+    } else if (/^\-?\d+(\.?\d+)?$/.test(this.state.resultExpression)) {
       this.setState({
-        resultExpression: this.state.resultExpression + operator.toLowerCase(), 
+        resultExpression: this.state.resultExpression + " " + operator.toLowerCase() + " ", 
         currentIntput: operator
       });
-    } else if (/\d+[\/x\-\+]$/.test(this.state.resultExpression)) {
+    } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] \-?$/.test(this.state.resultExpression)) {
       this.setState({
-        resultExpression: this.state.resultExpression.replace(/.$/, operator.toLowerCase()), 
+        resultExpression: this.state.resultExpression.replace(/. \-?$/, operator.toLowerCase() + " "), 
         currentIntput: operator
       });
+    } else if (/^\-?\d+(\.?\d+)? [\/x\-\+] \-?\d+(\.?\d+)?$/.test(this.state.resultExpression)) {
+      this.makeOperation(operator);
     }
   }
 
